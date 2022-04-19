7dce92cfdb8b72e0b54baf0d872994a7080d2e9c
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-04-19 17:50:31 -0500

    commit tree 345ee10a243c87fd968054e593c132e635e56f3a

--- src/index.js
+++ src/index.js
@@ -12,4 +12,66 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  toyFormContainer.addEventListener("submit", (event) => {
+    event.preventDefault()
+    postToy(event.target.name.value, event.target.image.value)
+   })
+   
 });
+
+function getToys() {
+fetch("http://localhost:3000/toys")
+.then(response => response.json())
+.then(response => response.map(createCard))
+}
+getToys()
+
+function createCard(toy) {
+
+let toyCollection = document.getElementById("toy-collection")
+let div = document.createElement("div")
+div.className = "card"
+toyCollection.append(div)
+
+const toyCard = `<h2>${toy.name}</h2>
+<img src=${toy.image} class="toy-avatar" />
+<p>${toy.likes} Likes</p>
+<button class="like-btn" id="[toy_id]">Like ❤️</button>`
+div.innerHTML += toyCard
+
+}
+
+
+function postToy(name, url) {
+  fetch("http://localhost:3000/toys", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": name,
+      "image": url,
+      "likes": 0
+    })
+  })
+  .then(response => response.json())
+  .then(data => createCard(data))
+}
+
+function patchToyLikes(id, newLikes) {
+  fetch(`http://localhost:3000/toys/:${id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "likes": likes
+    })
+  })
+}
+const likeButton = document.getElementById("[toy_id]")
+likeButton.addEventListener("click", (e) => {
+    console.log(e.target)
+})
